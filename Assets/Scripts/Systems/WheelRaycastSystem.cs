a5278ff34a17f380478176a4babf737e83f90a9d
Black heart <52672318+szymonm992@users.noreply.github.com>
2023-12-28 12:08:43 +0100

    Remove old commented code

--- Assets/Scripts/Systems/WheelRaycastSystem.cs
+++ Assets/Scripts/Systems/WheelRaycastSystem.cs
@@ -6,7 +6,6 @@ using Unity.Physics;
 using Unity.Physics.Extensions;
 using Unity.Physics.Systems;
 using Unity.Transforms;
-using UnityEditor;
 using UnityEngine;
 
 namespace ECSExperiment.Wheels
@@ -19,117 +18,16 @@ namespace ECSExperiment.Wheels
         public void OnUpdate(ref SystemState state)
         {
             var physicsWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().PhysicsWorld;
-           
-                var wheelRaycastJob = new WheelRaycastJob
-                {
-                    PhysicsWorld = physicsWorld,
-                    LocalTransformLookup = SystemAPI.GetComponentLookup<LocalTransform>(true),
-                    WorldTransformLookup = SystemAPI.GetComponentLookup<LocalToWorld>(true),
-                };
 
-                state.Dependency = wheelRaycastJob.Schedule(state.Dependency);
-            
-
-            /*
-            var physicsWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().PhysicsWorld;
-
-            foreach (var vehicleEntityProperties in SystemAPI.Query<RefRO<VehicleProperties>>())
+            var wheelRaycastJob = new WheelRaycastJob
             {
-                foreach (var (wheel, hitData) in SystemAPI.Query<RefRW<WheelProperties>, RefRW<WheelHitData>>())
-                {
+                PhysicsWorld = physicsWorld,
+                LocalTransformLookup = SystemAPI.GetComponentLookup<LocalTransform>(true),
+                WorldTransformLookup = SystemAPI.GetComponentLookup<LocalToWorld>(true),
+            };
 
-                    var wheelRaycastJob = new WheelRaycastJob()
-                    {
-                        PhysicsWorld = physicsWorld,
-                        //LocalTransformLookup = SystemAPI.GetComponentLookup<LocalTransform>(true),
-                        //WorldTransformLookup = SystemAPI.GetComponentLookup<LocalToWorld>(true),
-                        VehicleEntityProperties = vehicleEntityProperties.ValueRO,
-                    };
-
-                    state.Dependency = wheelRaycastJob.Schedule(state.Dependency);
-                }
-            }
-        }*/
+            state.Dependency = wheelRaycastJob.Schedule(state.Dependency);
         }
-        /*
-        [BurstCompile]
-        [WithAll(typeof(Simulate))]
-        public partial struct WheelRaycastJob : IJobEntity
-        {
-            //[ReadOnly] public ComponentLookup<LocalTransform> LocalTransformLookup;
-            //[ReadOnly] public ComponentLookup<LocalToWorld> WorldTransformLookup;
-
-            public PhysicsWorld PhysicsWorld;
-           
-
-            private void Execute()
-            {
-
-                var wheelTransform = WorldTransformLookup[wheelProperties.Entity];
-                var vehicleTransform = LocalTransformLookup[VehicleEntityProperties.VehicleEntity];
-
-                float3 worldUpDirection = new float3(0, 1, 0);
-                float3 localUpDirection = math.mul(vehicleTransform.Rotation, worldUpDirection);
-
-                var rigidbodyIndex = PhysicsWorld.GetRigidBodyIndex(VehicleEntityProperties.VehicleEntity);
-                var rayStart = wheelTransform.Position + (localUpDirection * wheelProperties.Radius);
-                var rayEnd = rayStart - localUpDirection * (wheelProperties.SpringLength + wheelProperties.Radius);
-                var rayCollisionFilter = PhysicsWorld.GetCollisionFilter(rigidbodyIndex);
-
-                var raycastInput = new RaycastInput
-                {
-                    Start = rayStart,
-                    End = rayEnd,
-                    Filter = rayCollisionFilter
-                };
-
-                wheelHitData.WheelCenter = rayStart - (localUpDirection * wheelProperties.SpringLength);
-                wheelHitData.Velocity = PhysicsWorld.GetLinearVelocity(rigidbodyIndex, wheelHitData.WheelCenter);
-                wheelHitData.HasHit = false;
-
-                wheelProperties.IsGrounded = PhysicsWorld.CollisionWorld.CastRay(raycastInput, out var result);
-
-    #if UNITY_EDITOR
-                Color rayColor = wheelProperties.IsGrounded ? Color.green : Color.red;
-                Debug.DrawRay(rayStart, rayEnd - rayStart, rayColor);
-    #endif
-
-                if (wheelProperties.IsGrounded)
-                {
-                    float raycastDistance = math.distance(result.Position, rayStart);
-
-                    wheelHitData.HasHit = true;
-                    wheelHitData.HitPoint = result.Position;
-                    wheelHitData.SurfaceFriction = result.Material.Friction;
-                    wheelHitData.Distance = (raycastDistance - wheelProperties.Radius);
-                    wheelHitData.WheelCenter = rayStart - (localUpDirection * raycastDistance);
-
-    #if UNITY_EDITOR
-                    Color color = wheelProperties.IsGrounded ? Color.green : Color.red;
-                    float3 localRightDirection = math.mul(wheelTransform.Rotation,
-                        new float3((int)wheelProperties.Side, 0, 0));
-                    Debug.DrawRay(wheelHitData.WheelCenter, localRightDirection, color);
-    #endif
-
-                    var velocityAtWheel = PhysicsWorld.GetLinearVelocity(rigidbodyIndex, wheelHitData.WheelCenter);
-                    var invertedWheelsCount = (1f / VehicleEntityProperties.WheelsAmount);
-                    var currentSpeedUp = math.dot(velocityAtWheel, localUpDirection);
-
-                    float3 lvA = currentSpeedUp * localUpDirection;
-                    float3 lvB = PhysicsWorld.GetLinearVelocity(result.RigidBodyIndex, result.Position);
-                    float3 totalSuspensionForce = (wheelProperties.Spring * (result.Position - rayEnd))
-                        + (wheelProperties.Damper * (lvB - lvA)) * invertedWheelsCount;
-
-                    float impulseUp = math.dot(totalSuspensionForce, localUpDirection);
-                    float downForceLimit = -0.25f;
-
-                    if (downForceLimit < impulseUp)
-                    {
-                        totalSuspensionForce = impulseUp * localUpDirection;
-                        PhysicsWorld.ApplyImpulse(rigidbodyIndex, totalSuspensionForce, wheelTransform.Position);
-                    }
-                }
-            }*/
     }
 
     [BurstCompile]
