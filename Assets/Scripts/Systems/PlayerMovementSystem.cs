9565b810f01b4daa179ab4f1cc4b983850e8cd09
Black heart <szymon.m992@gmail.com>
2024-07-17 21:08:33 +0200

    Fix vehicle friction and raycast system

--- Assets/Scripts/Systems/PlayerMovementSystem.cs
+++ Assets/Scripts/Systems/PlayerMovementSystem.cs
@@ -7,6 +7,7 @@ using Unity.Physics.Extensions;
 using Unity.Physics.Systems;
 using Unity.Transforms;
 using UnityEngine;
+using System.Collections.Generic;
 
 [UpdateInGroup(typeof(PhysicsSimulationGroup))]
 public partial struct PlayerMovementSystem : ISystem
@@ -16,6 +17,7 @@ public partial struct PlayerMovementSystem : ISystem
     {
         var physicsWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().PhysicsWorld;
 
+<<<<<<< Updated upstream
         foreach (var (inputs, properties) in SystemAPI.Query<RefRO<InputsData>, RefRO<VehicleProperties>>())
         {
             var vehicleDriveJob = new DriveJob
@@ -27,6 +29,95 @@ public partial struct PlayerMovementSystem : ISystem
                 FixedTime = SystemAPI.Time.fixedDeltaTime,
             };
             state.Dependency = vehicleDriveJob.Schedule(state.Dependency);
+=======
+        /*foreach (var (wheelProperties, hitData, wheelTransform) in SystemAPI.Query<RefRW<WheelProperties>, RefRO<WheelHitData>, RefRO<LocalToWorld>>())
+        {  
+            var rigidbodyIndex = physicsWorld.GetRigidBodyIndex(wheelProperties.ValueRO.VehicleEntity);
+
+            if (wheelProperties.ValueRO.IsGrounded)
+            {
+                wheelProperties.ValueRW.compression = 1.0f - ((hitData.ValueRO.Distance - wheelProperties.ValueRO.Radius) / wheelProperties.ValueRO.SpringLength);
+                wheelProperties.ValueRW.wheelVelo = physicsWorld.GetLinearVelocity(rigidbodyIndex, hitData.ValueRO.WheelCenter);
+                wheelProperties.ValueRW.localVelo = math.mul(wheelProperties.ValueRO.inverseLocalRotation, wheelProperties.ValueRW.wheelVelo);
+
+                float normalForce = wheelProperties.ValueRW.compression * wheelProperties.ValueRW.fullCompressionSpringForce;
+                wheelProperties.ValueRW.roadForce = RoadForce(ref state, wheelProperties, hitData, normalForce, wheelTransform.ValueRO);
+
+                physicsWorld.ApplyImpulse(rigidbodyIndex, wheelProperties.ValueRW.roadForce, hitData.ValueRO.WheelCenter);
+            }
+            else
+            {
+                wheelProperties.ValueRW.compression = 0.0f;
+                wheelProperties.ValueRW.roadForce = float3.zero;
+
+                float totalInertia = wheelProperties.ValueRO.inertia + wheelProperties.ValueRO.drivetrainInertia;
+                float driveAngularDelta = wheelProperties.ValueRO.driveTorque * SystemAPI.Time.DeltaTime / totalInertia;
+                float totalFrictionTorque = wheelProperties.ValueRO.brakeFrictionTorque * wheelProperties.ValueRO.brake +
+                    wheelProperties.ValueRO.handbrakeFrictionTorque * wheelProperties.ValueRO.handbrake + wheelProperties.ValueRO.frictionTorque + wheelProperties.ValueRO.driveFrictionTorque;
+                float frictionAngularDelta = totalFrictionTorque * SystemAPI.Time.DeltaTime / totalInertia;
+
+                wheelProperties.ValueRW.angularVelocity += driveAngularDelta;
+
+                if (math.abs(wheelProperties.ValueRW.angularVelocity) > frictionAngularDelta)
+                {
+                    wheelProperties.ValueRW.angularVelocity -= frictionAngularDelta * math.sign(wheelProperties.ValueRW.angularVelocity);
+                }
+                else
+                {
+                    wheelProperties.ValueRW.angularVelocity = 0;
+                }
+
+                wheelProperties.ValueRW.slipRatio = 0;
+                wheelProperties.ValueRW.slipVelo = 0;
+            }
+
+            wheelProperties.ValueRW.compression = Mathf.Clamp01(wheelProperties.ValueRW.compression);
+            */
+
+
+        foreach (var (wheelProperties, hitData, wheelLocalTransform) in SystemAPI.Query<RefRW<WheelProperties>, RefRO<WheelHitData>, RefRO<LocalTransform>>())
+        {
+            var rigidbodyIndex = physicsWorld.GetRigidBodyIndex(wheelProperties.ValueRO.VehicleEntity);
+
+            var vehicleRigidbodyTransform = LocalTransform.FromMatrix(float4x4.TRS(
+                    PhysicsWorldExtensions.GetPosition(physicsWorld, rigidbodyIndex),
+                    PhysicsWorldExtensions.GetRotation(physicsWorld, rigidbodyIndex),
+                    new float3(1f, 1f, 1f)
+                ));
+
+            var wheelCastOriginGlobalTransform = vehicleRigidbodyTransform.TransformTransform(wheelLocalTransform.ValueRO);
+
+            float FRICTION_GRIP_FACTOR = 1f;
+
+            float3 localRightDirection = math.mul(wheelCastOriginGlobalTransform.Rotation, math.right());
+            float3 localForwardDirection = math.forward(wheelCastOriginGlobalTransform.Rotation);
+            float3 springDir = math.mul(wheelCastOriginGlobalTransform.Rotation, math.up());
+            var tireVel = physicsWorld.GetLinearVelocity(rigidbodyIndex, hitData.ValueRO.WheelCenter);
+
+            float steeringVel = math.dot(localRightDirection, tireVel);
+            float desiredSidewaysVelChange = -steeringVel * FRICTION_GRIP_FACTOR;
+            float desiredSidewaysAccel = desiredSidewaysVelChange / SystemAPI.Time.DeltaTime;
+
+            if (wheelProperties.ValueRO.IsGrounded)
+            {
+                
+                physicsWorld.ApplyImpulse(rigidbodyIndex, desiredSidewaysAccel * localRightDirection, hitData.ValueRO.WheelCenter);
+            }
+
+            float forwardVel = math.dot(localForwardDirection, tireVel);
+            float desiredForwardVelChange = -forwardVel * FRICTION_GRIP_FACTOR;
+            float desiredForwardAccel = desiredForwardVelChange / SystemAPI.Time.DeltaTime;
+
+            if (wheelProperties.ValueRO.IsGrounded)
+            {
+                
+                physicsWorld.ApplyImpulse(rigidbodyIndex, desiredForwardAccel * localForwardDirection, hitData.ValueRO.WheelCenter);
+            }
+
+
+            Debug.DrawRay(hitData.ValueRO.HitPoint, localForwardDirection, Color.blue);
+            Debug.DrawRay(hitData.ValueRO.HitPoint, localRightDirection, Color.red);
+>>>>>>> Stashed changes
         }
     }
 }
--- Assets/Scripts/Systems/WheelRaycastSystem.cs
+++ Assets/Scripts/Systems/WheelRaycastSystem.cs
@@ -7,39 +7,121 @@ using Unity.Physics.Extensions;
 using Unity.Physics.Systems;
 using Unity.Transforms;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace ECSExperiment.Wheels
 {
+<<<<<<< Updated upstream
     [UpdateInGroup(typeof(PhysicsSimulationGroup))]
     [UpdateBefore(typeof(PlayerMovementSystem))]
+=======
+    [UpdateInGroup(typeof(PhysicsSimulationGroup), OrderFirst = true)]
+>>>>>>> Stashed changes
     public partial struct WheelRaycastSystem : ISystem
     {
-        [BurstCompile]
+   
         public void OnUpdate(ref SystemState state)
         {
+            state.Dependency.Complete();
             var physicsWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().PhysicsWorld;
 
-            var wheelRaycastJob = new WheelRaycastJob
-            {
-                PhysicsWorld = physicsWorld,
-                LocalTransformLookup = SystemAPI.GetComponentLookup<LocalTransform>(true),
-                WorldTransformLookup = SystemAPI.GetComponentLookup<LocalToWorld>(true),
-            };
 
-            state.Dependency = wheelRaycastJob.Schedule(state.Dependency);
+            foreach (var (wheelProperties, wheelHitData, wheelCastOriginLocalTransform) in SystemAPI.Query<RefRW<WheelProperties>, RefRW<WheelHitData>, RefRO<LocalTransform>>())
+            {
+                var rigidbodyIndex = physicsWorld.GetRigidBodyIndex(wheelProperties.ValueRO.VehicleEntity);
+
+                if (-1 == rigidbodyIndex || rigidbodyIndex >= physicsWorld.NumDynamicBodies)
+                {
+                    return;
+                }
+
+                var vehicleRigidbodyTransform = LocalTransform.FromMatrix(float4x4.TRS(
+                        PhysicsWorldExtensions.GetPosition(physicsWorld, rigidbodyIndex),
+                        PhysicsWorldExtensions.GetRotation(physicsWorld, rigidbodyIndex),
+                        new float3(1f, 1f, 1f)
+                    ));
+
+                var wheelCastOriginGlobalTransform = vehicleRigidbodyTransform.TransformTransform(wheelCastOriginLocalTransform.ValueRO);
+                var springDirection = math.mul(wheelCastOriginGlobalTransform.Rotation, math.up());
+                var wheelRightLocal = math.mul(wheelCastOriginGlobalTransform.Rotation, math.right());
+
+                float3 localUpDirection = math.mul(wheelCastOriginGlobalTransform.Rotation, math.up());
+
+                var rayStart = wheelCastOriginGlobalTransform.Position + (localUpDirection * wheelProperties.ValueRO.Radius);
+                var rayEnd = rayStart - localUpDirection * (wheelProperties.ValueRO.SpringLength + wheelProperties.ValueRO.Radius);
+
+                var raycastInput = new RaycastInput
+                {
+                    Start = rayStart,
+                    End = rayEnd,
+                    Filter = CollisionFilter.Default,
+                };
+
+                wheelHitData.ValueRW.WheelCenter = rayStart - (localUpDirection * wheelProperties.ValueRO.SpringLength);
+                wheelHitData.ValueRW.Velocity = physicsWorld.GetLinearVelocity(rigidbodyIndex, wheelHitData.ValueRO.WheelCenter);
+                wheelHitData.ValueRW.HasHit = false;
+
+                Color rayColor = wheelProperties.ValueRO.IsGrounded ? Color.green : Color.red;
+                Debug.DrawRay(rayStart, rayEnd - rayStart, rayColor);
+
+                if (physicsWorld.CollisionWorld.CastRay(raycastInput, out var result))
+                {
+                    float raycastDistance = math.distance(result.Position, rayStart);
+
+                    wheelProperties.ValueRW.IsGrounded = true;
+                    wheelHitData.ValueRW.HasHit = true;
+                    wheelHitData.ValueRW.HitPoint = result.Position;
+                    wheelHitData.ValueRW.SurfaceFriction = result.Material.Friction;
+                    wheelHitData.ValueRW.Normal = result.SurfaceNormal;
+                    wheelHitData.ValueRW.Distance = (raycastDistance - wheelProperties.ValueRO.Radius);
+                    wheelHitData.ValueRW.WheelCenter = rayStart - (localUpDirection * raycastDistance);
+
+
+                    var velocityAtWheel = physicsWorld.GetLinearVelocity(rigidbodyIndex, wheelHitData.ValueRO.WheelCenter);
+                    var invertedWheelsCount = (1f / 4f);
+                    var currentSpeedUp = math.dot(velocityAtWheel, localUpDirection);
+
+                    float3 lvA = currentSpeedUp * localUpDirection;
+                    float3 lvB = physicsWorld.GetLinearVelocity(result.RigidBodyIndex, wheelHitData.ValueRO.WheelCenter);
+                    float3 totalSuspensionForce = (wheelProperties.ValueRO.Spring * (result.Position - rayEnd))
+                        + (wheelProperties.ValueRO.Damper * (lvB - lvA)) * invertedWheelsCount;
+
+                    Debug.DrawRay(wheelHitData.ValueRO.WheelCenter, math.up(), Color.yellow);
+                    Debug.DrawRay(wheelHitData.ValueRO.WheelCenter, localUpDirection, Color.cyan);
+
+                    float impulseUp = math.dot(totalSuspensionForce, localUpDirection);
+                    float downForceLimit = -0.25f;
+
+                    if (downForceLimit < impulseUp)
+                    {
+                        totalSuspensionForce = impulseUp * localUpDirection;
+                        physicsWorld.ApplyImpulse(rigidbodyIndex, totalSuspensionForce, wheelHitData.ValueRO.WheelCenter);
+                    }
+                }
+                else
+                {
+                    wheelProperties.ValueRW.IsGrounded = false;
+                }
+
+#if UNITY_EDITOR
+                Color color = wheelProperties.ValueRO.IsGrounded ? Color.green : Color.red;
+                float3 localRightDirection = math.mul(wheelCastOriginGlobalTransform.Rotation,
+                    new float3((int)wheelProperties.ValueRO.Side, 0, 0));
+                Debug.DrawRay(wheelHitData.ValueRO.WheelCenter, localRightDirection, color);
+            }
+           
         }
     }
 
-    [BurstCompile]
     [WithAll(typeof(Simulate))]
     public partial struct WheelRaycastJob : IJobEntity
     {
         public PhysicsWorld PhysicsWorld;
         [ReadOnly] public ComponentLookup<LocalTransform> LocalTransformLookup;
-        [ReadOnly] public ComponentLookup<LocalToWorld> WorldTransformLookup;
 
         private void Execute(ref WheelProperties wheelProperties, ref WheelHitData wheelHitData)
         {
+<<<<<<< Updated upstream
             var wheelTransform = WorldTransformLookup[wheelProperties.Entity];
             var vehicleTransform = LocalTransformLookup[wheelProperties.VehicleEntity];
 
@@ -106,6 +188,10 @@ namespace ECSExperiment.Wheels
                     PhysicsWorld.ApplyImpulse(rigidbodyIndex, totalSuspensionForce, wheelTransform.Position);
                 }
             }
+=======
+            
+#endif
+>>>>>>> Stashed changes
         }
     }
 }
