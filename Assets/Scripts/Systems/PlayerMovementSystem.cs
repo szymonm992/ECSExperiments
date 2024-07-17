9d0b0f362e163c0943524d5abc5f3c925b9e2699
Black heart <szymon.m992@gmail.com>
2024-07-17 21:18:30 +0200

    Restore changes

--- Assets/Scripts/Systems/PlayerMovementSystem.cs
+++ Assets/Scripts/Systems/PlayerMovementSystem.cs
@@ -17,19 +17,6 @@ public partial struct PlayerMovementSystem : ISystem
     {
         var physicsWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().PhysicsWorld;
 
-<<<<<<< Updated upstream
-        foreach (var (inputs, properties) in SystemAPI.Query<RefRO<InputsData>, RefRO<VehicleProperties>>())
-        {
-            var vehicleDriveJob = new DriveJob
-            {
-                PhysicsWorld = physicsWorld,
-                LocalTransformLookup = SystemAPI.GetComponentLookup<LocalTransform>(true),
-                InputsData = inputs.ValueRO,
-                VehicleProperties = properties.ValueRO,
-                FixedTime = SystemAPI.Time.fixedDeltaTime,
-            };
-            state.Dependency = vehicleDriveJob.Schedule(state.Dependency);
-=======
         /*foreach (var (wheelProperties, hitData, wheelTransform) in SystemAPI.Query<RefRW<WheelProperties>, RefRO<WheelHitData>, RefRO<LocalToWorld>>())
         {  
             var rigidbodyIndex = physicsWorld.GetRigidBodyIndex(wheelProperties.ValueRO.VehicleEntity);
@@ -100,7 +87,7 @@ public partial struct PlayerMovementSystem : ISystem
 
             if (wheelProperties.ValueRO.IsGrounded)
             {
-                
+
                 physicsWorld.ApplyImpulse(rigidbodyIndex, desiredSidewaysAccel * localRightDirection, hitData.ValueRO.WheelCenter);
             }
 
@@ -110,83 +97,15 @@ public partial struct PlayerMovementSystem : ISystem
 
             if (wheelProperties.ValueRO.IsGrounded)
             {
-                
+
                 physicsWorld.ApplyImpulse(rigidbodyIndex, desiredForwardAccel * localForwardDirection, hitData.ValueRO.WheelCenter);
             }
 
 
             Debug.DrawRay(hitData.ValueRO.HitPoint, localForwardDirection, Color.blue);
             Debug.DrawRay(hitData.ValueRO.HitPoint, localRightDirection, Color.red);
->>>>>>> Stashed changes
         }
     }
 }
-
-[BurstCompile]
-[WithAll(typeof(Simulate))]
-public partial struct DriveJob : IJobEntity
-{
-    [ReadOnly] public ComponentLookup<LocalTransform> LocalTransformLookup;
-    [ReadOnly] public InputsData InputsData;
-    [ReadOnly] public VehicleProperties VehicleProperties;
-
-    public PhysicsWorld PhysicsWorld;
-    public float FixedTime;
-
-    private void Execute(in WheelProperties wheelProperties, in WheelHitData hitData)
-    {
-        bool isInputPositive = InputsData.Vertical.IsNumberPositive();
-
-        var rigidbodyIndex = PhysicsWorld.GetRigidBodyIndex(wheelProperties.VehicleEntity);
-        var currentMaxSpeed = isInputPositive ? VehicleProperties.VehicleMaximumForwardSpeed : VehicleProperties.VehicleMaximumBackwardSpeed;
-        LocalTransform vehicleTransform = LocalTransformLookup[wheelProperties.VehicleEntity];
-
-        float3 worldUpDirection = new float3(0, 1, 0);
-        //float3 localUpDirection = math.mul(vehicleTransform.Rotation, worldUpDirection);
-        float3 gravityImpulse = -9.81f * worldUpDirection;
-        PhysicsWorld.ApplyImpulse(rigidbodyIndex, gravityImpulse, vehicleTransform.Position);
-
-        if (!wheelProperties.IsGrounded || !wheelProperties.CanDrive)
-        {
-            return;
-        }
-
-        float3 worldForwardDirection = new float3(0, 0, 1);
-        float3 localForwardDirection = math.mul(vehicleTransform.Rotation, worldForwardDirection);
-
-        if (VehicleProperties.CurrentSpeed <= currentMaxSpeed)
-        {
-            float impulsePower = InputsData.Vertical * (currentMaxSpeed * 0.5f);
-            float3 impulse = impulsePower * localForwardDirection;
-
-            PhysicsWorld.ApplyImpulse(rigidbodyIndex, impulse, hitData.HitPoint);
-        }
-
-        float FRICTION_GRIP_FACTOR = 1f;
-        float3 localRightDirection = math.mul(vehicleTransform.Rotation, new float3(-1, 0, 0));
-
-        var tireVel = PhysicsWorld.GetLinearVelocity(rigidbodyIndex, hitData.HitPoint);
-        
-        float steeringVel = math.dot(localRightDirection, tireVel);
-        float desiredSidewaysVelChange = -steeringVel * FRICTION_GRIP_FACTOR;
-        float desiredSidewaysAccel = desiredSidewaysVelChange / FixedTime;
-
-        PhysicsWorld.ApplyImpulse(rigidbodyIndex, desiredSidewaysAccel * localRightDirection, hitData.HitPoint);
-
-
-        tireVel = PhysicsWorld.GetLinearVelocity(rigidbodyIndex, hitData.HitPoint);
-
-        float forwardVel = math.dot(localForwardDirection, tireVel);
-        float desiredForwardVelChange = -forwardVel * FRICTION_GRIP_FACTOR;
-        float desiredForwardAccel = desiredForwardVelChange / FixedTime;
-
-        PhysicsWorld.ApplyImpulse(rigidbodyIndex, desiredForwardAccel * localForwardDirection, hitData.HitPoint);
-
-        Debug.DrawRay(hitData.HitPoint, localForwardDirection, Color.blue);
-        Debug.DrawRay(hitData.HitPoint, localRightDirection, Color.red);
-
-    }
-}
-
 
 
