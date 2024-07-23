d76543ae19e7352966efe459e95e228ce926d0ca
unforg1v3n <szymon.m992@gmail.com>
2024-07-23 21:15:06 +0200

    Naming changes

--- Assets/Scripts/AuthoringAnMono/WheelJoint.cs
+++ Assets/Scripts/AuthoringAnMono/WheelJoint.cs
@@ -3,16 +3,13 @@ using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Authoring;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 namespace ECSExperiment.Wheels
 {
-    public class CustomWheelJoint : BaseJoint
+    public class WheelJoint : BaseJoint
     {
         [Tooltip("An offset from the center of the body with the motor, representing the anchor point of translation.")]
         public float3 AnchorPosition;
-        [Tooltip("The direction of the motor, relative to the orientation of the Connected Body (bodyB). Value will be normalized")]
-        public float3 DirectionOfMovement;
         [Tooltip("Motor will drive this length away from the anchor position of bodyA.")]
         public float TargetDistance;
         [Tooltip("The magnitude of the maximum impulse the motor can exert in a single step. Applies only to the motor constraint.")]
@@ -25,22 +22,20 @@ namespace ECSExperiment.Wheels
         private float3 AxisInConnectedEntity;
         private float3 PerpendicularAxisInConnectedEntity;
 
-        class CustomWheelJointAuthoring : JointBaker<CustomWheelJoint>
+        class CustomWheelJointAuthoring : JointBaker<WheelJoint>
         {
-            public override void Bake(Wheels.CustomWheelJoint authoring)
+            public override void Bake(WheelJoint authoring)
             {
-                float3 axisInB = math.normalize(authoring.DirectionOfMovement);
+                var springDirection = math.up();
+                var aFromB = math.mul(math.inverse(authoring.worldFromA), authoring.worldFromB);
+                var axisInA = math.mul(aFromB.rot, springDirection);
+                var bFromA = math.mul(math.inverse(authoring.worldFromB), authoring.worldFromA);
 
-                RigidTransform aFromB = math.mul(math.inverse(authoring.worldFromA), authoring.worldFromB);
-                float3 axisInA = math.mul(aFromB.rot, axisInB); //motor axis relative to bodyA
+                authoring.PositionInConnectedEntity = math.transform(bFromA, authoring.AnchorPosition);
+                authoring.AxisInConnectedEntity = springDirection; 
 
-                RigidTransform bFromA = math.mul(math.inverse(authoring.worldFromB), authoring.worldFromA);
-                authoring.PositionInConnectedEntity = math.transform(bFromA, authoring.AnchorPosition); //position of motored body relative to Connected Entity in world space
-                authoring.AxisInConnectedEntity = axisInB; //motor axis in Connected Entity space
-
-                // Always calculate the perpendicular axes
                 Math.CalculatePerpendicularNormalized(axisInA, out var perpendicularLocal, out _);
-                authoring.PerpendicularAxisInConnectedEntity = math.mul(bFromA.rot, perpendicularLocal); //perp motor axis in Connected Entity space
+                authoring.PerpendicularAxisInConnectedEntity = math.mul(bFromA.rot, perpendicularLocal);
 
                 var joint = PhysicsJoint.CreatePositionMotor(
                     new BodyFrame
@@ -62,9 +57,7 @@ namespace ECSExperiment.Wheels
                 var constrains = joint.GetConstraints();
 
                 constrains[0] = Constraint.MotorPlanar(authoring.TargetDistance, math.abs(authoring.MaxImpulseAppliedByMotor), authoring.SpringFrequency, authoring.SpringDampening);
-
                 joint.SetConstraints(constrains);
-
                 joint.SetImpulseEventThresholdAllConstraints(authoring.MaxImpulse);
 
                 var constraintBodyPair = GetConstrainedBodyPair(authoring);
